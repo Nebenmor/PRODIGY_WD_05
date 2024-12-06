c5591baa3e4d9c3578a34adbf281e64254c3a4f6
Nebenmor <nebenmor.anthony@gmail.com>
2024-12-06 19:28:57 +0100

    update JS

--- script.js
+++ script.js
@@ -15,5 +15,20 @@ search.addEventListener('click', () => {
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
 
-    
+        if (json.cod == '404') {
+            cityHide.textContent = city;
+            container.style.height = '400px';
+            weatherBox.classList.remove('active');
+            weatherDetails.classList.remove('active');
+            error404.classList.add('active');
+            return;
+        }
+
+        container.style.height = '555px';
+        weatherBox.classList.add('active');
+        weatherDetails.classList.add('active');
+        error404.classList.remove('active');
+
+        
+            });
 });
