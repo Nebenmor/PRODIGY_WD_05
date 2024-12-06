9991fb75f1c690b6d766db7b8b9b91f85c8641db
Nebenmor <nebenmor.anthony@gmail.com>
2024-12-06 19:30:58 +0100

    update JS

--- script.js
+++ script.js
@@ -80,7 +80,14 @@ search.addEventListener('click', () => {
                     image.src = './images/cloud.png';
             }
     
-               
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>oC</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+            
+            }
+        }      
 
             });
 });
